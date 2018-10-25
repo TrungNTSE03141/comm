da45853cc6987376cbd2ae7f33c572e1cac3643a
Nguyen Tran Trung <ngguyentrantrung@hotmail.com>
2018-10-25 10:11:55 +0700

    Updates

---
+++ precache-manifest.5adba1e5d0d0a0476b930423d4799ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54eb94129c654b11bc63",
+    "url": "/comm/static/css/main.c583501b.chunk.css"
+  },
+  {
+    "revision": "54eb94129c654b11bc63",
+    "url": "/comm/static/js/main.54eb9412.chunk.js"
+  },
+  {
+    "revision": "9a62a2fee3c684362782",
+    "url": "/comm/static/css/1.04abf2da.chunk.css"
+  },
+  {
+    "revision": "9a62a2fee3c684362782",
+    "url": "/comm/static/js/1.9a62a2fe.chunk.js"
+  },
+  {
+    "revision": "b8e305bba73b4a334680",
+    "url": "/comm/static/js/runtime~main.b8e305bb.js"
+  },
+  {
+    "revision": "c7b3bb94684443a454324d775fe2cd00",
+    "url": "/comm/index.html"
+  }
+];
